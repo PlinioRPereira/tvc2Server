4ff90a956807cce8b72d1c5a01f047938891da6e
Luiz Luz <luizluz@dcc.ufmg.br>
2023-10-26 21:51:49 -0300

    Adicionando um menu para melhorar um pouco a experiência de uso.

--- clientTCP.py
+++ clientTCP.py
@@ -2,43 +2,75 @@ import socket
 import json
 import wave
 import numpy as np
+import threading
 
-SERVER_IP = "127.0.0.1"
-SERVER_PORT = 5000
-WAV_OUTPUT_FILENAME = "output.wav"
+def start_recording():
+    SERVER_IP = "127.0.0.1"
+    SERVER_PORT = 5000
+    WAV_OUTPUT_FILENAME = "output.wav"
 
-# Inicializando o socket TCP
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((SERVER_IP, SERVER_PORT))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((SERVER_IP, SERVER_PORT))
 
-# Receber metadados do servidor
-metadata_bytes = client_socket.recv(1024)
-metadata = json.loads(metadata_bytes.decode('utf-8'))
-RATE = metadata['rate']
-CHANNELS = metadata['channels']
-DTYPE = np.dtype(metadata['dtype'])  # Agora deve ser interpretável
-BUFFER_SIZE = metadata['buffer_size']
+    metadata_bytes = client_socket.recv(1024)
+    metadata = json.loads(metadata_bytes.decode('utf-8'))
+    RATE = metadata['rate']
+    CHANNELS = metadata['channels']
+    DTYPE = np.dtype(metadata['dtype'])
+    BUFFER_SIZE = metadata['buffer_size']
+
+    wav_file = wave.open(WAV_OUTPUT_FILENAME, 'wb')
+    wav_file.setnchannels(CHANNELS)
+    wav_file.setsampwidth(DTYPE.itemsize)
+    wav_file.setframerate(RATE)
+
+    print(f"Gravando com {RATE} taxa de amostragem, {CHANNELS} canais, buffer de tamanho {BUFFER_SIZE}.")
+
+    try:
+        while recording:
+            audio_data = client_socket.recv(BUFFER_SIZE)
+            if not audio_data:
+                break
+            wav_file.writeframes(audio_data)
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+    finally:
+        wav_file.close()
+        client_socket.close()
 
 
+# Variável global para controlar o estado da gravação
+recording = False
 
-# Inicializando o arquivo WAV
-wav_file = wave.open(WAV_OUTPUT_FILENAME, 'wb')
-wav_file.setnchannels(CHANNELS)
-wav_file.setsampwidth(DTYPE.itemsize)
-wav_file.setframerate(RATE)
+# Interface de menu
+while True:
+    print("\nMenu:")
+    if not recording:
+        print("1. Iniciar gravação")
+    else:
+        print("2. Parar gravação")
+    print("3. Sair")
 
-print(f"Gravando com {RATE} taxa de amostragem, {CHANNELS} canais, buffer de tamanho {BUFFER_SIZE}.")
+    choice = input("Escolha uma opção: ")
 
-try:
-    while True:
-        audio_data = client_socket.recv(BUFFER_SIZE)
-        if not audio_data:
-            break
-        wav_file.writeframes(audio_data)
-except KeyboardInterrupt:
-    print("Gravação interrompida pelo usuário.")
-except Exception as e:
-    print(f"Ocorreu um erro: {e}")
-finally:
-    wav_file.close()
-    client_socket.close()
+    if choice == '1':
+        if not recording:
+            recording = True
+            thread = threading.Thread(target=start_recording)
+            thread.start()
+            print("Gravação iniciada.")
+        else:
+            print("Gravação já está em andamento.")
+    elif choice == '2':
+        if recording:
+            recording = False
+            print("Gravação parada.")
+        else:
+            print("Nenhuma gravação em andamento.")
+    elif choice == '3':
+        if recording:
+            recording = False
+        print("Saindo.")
+        break
+    else:
+        print("Opção inválida.")
--- publishAudioTCP.py
+++ publishAudioTCP.py
@@ -2,41 +2,81 @@ import json
 import sounddevice as sd
 import numpy as np
 import socket
+import threading
 
 def get_device_info(device_id):
     device_info = sd.query_devices(device_id, 'input')
     return device_info['default_samplerate'], np.int16
 
-DEVICE_ID = 2  # ID do dispositivo de entrada
-CHANNELS = 2  # Número de canais deve ser sempre 2
-RATE, DTYPE = get_device_info(DEVICE_ID)
-BUFFER_SIZE = 1024  # Tamanho do buffer
+def start_streaming(device_id, rate):
+    CHANNELS = 2
+    DTYPE = np.int16
+    BUFFER_SIZE = 1024
 
-# Inicializando o socket TCP
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0.0.0.0', 5000))
-server_socket.listen(1)
-conn, addr = server_socket.accept()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 5000))
+    server_socket.listen(1)
+    conn, addr = server_socket.accept()
 
-print(f"Conexão estabelecida com {addr}")
+    print(f"Conexão estabelecida com {addr}")
 
-# Enviar metadados para o cliente
-metadata = {
-    'rate': RATE,
-    'channels': CHANNELS,
-    'dtype': 'int16',  # Especifica o dtype como string
-    'buffer_size': BUFFER_SIZE
-}
-conn.sendall(json.dumps(metadata).encode('utf-8'))
+    metadata = {
+        'rate': rate,
+        'channels': CHANNELS,
+        'dtype': 'int16',
+        'buffer_size': BUFFER_SIZE
+    }
+    conn.sendall(json.dumps(metadata).encode('utf-8'))
 
-try:
-    with sd.InputStream(samplerate=RATE, channels=CHANNELS, dtype=DTYPE, device=DEVICE_ID) as stream:
-        while True:
-            audio_chunk, overflowed = stream.read(BUFFER_SIZE)
-            audio_bytes = audio_chunk.astype(DTYPE).tobytes()
-            conn.sendall(audio_bytes)
-except Exception as e:
-    print(f"Ocorreu um erro: {e}")
-finally:
-    conn.close()
-    server_socket.close()
+    try:
+        with sd.InputStream(samplerate=rate, channels=CHANNELS, dtype=DTYPE, device=device_id) as stream:
+            while streaming:
+                audio_chunk, overflowed = stream.read(BUFFER_SIZE)
+                audio_bytes = audio_chunk.astype(DTYPE).tobytes()
+                conn.sendall(audio_bytes)
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+    finally:
+        conn.close()
+        server_socket.close()
+
+devices = sd.query_devices()
+
+streaming = False
+
+while True:
+    print("\nMenu:")
+    if not streaming:
+        print("1. Iniciar transmissão")
+    else:
+        print("2. Parar transmissão")
+    print("3. Sair")
+    choice = input("Escolha uma opção: ")
+
+    if choice == '1':
+        if not streaming:
+            for i, device in enumerate(devices):
+                print(
+                    f"ID: {i}, Nome: {device['name']}, Canais: {device['max_input_channels']}, Taxas de amostragem suportadas: {device['default_samplerate']}")
+
+            device_id = int(input("Informe o ID do dispositivo: "))
+            rate = int(input("Informe a taxa de amostragem: "))
+            streaming = True
+            thread = threading.Thread(target=start_streaming, args=(device_id, rate))
+            thread.start()
+            print("Transmissão iniciada.")
+        else:
+            print("Transmissão já está em andamento.")
+    elif choice == '2':
+        if streaming:
+            streaming = False
+            print("Transmissão parada.")
+        else:
+            print("Nenhuma transmissão em andamento.")
+    elif choice == '3':
+        if streaming:
+            streaming = False
+        print("Saindo.")
+        break
+    else:
+        print("Opção inválida.")
